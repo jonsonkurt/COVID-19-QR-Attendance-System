ea0f08e31d6edb118bcbacbdeef87a9ea2bc6f79
_jonsonkurt <jonsonkurt@gmail.com>
2022-01-02 00:59:23 +0800

    Updated homescreen features

--- libs/baseclass/homescreen.py
+++ libs/baseclass/homescreen.py
@@ -1,17 +1,12 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivymd.toast.kivytoast import toast
-from kivy.uix.screenmanager import NoTransition, Screen
+from kivy.uix.screenmanager import Screen
 from libs.baseclass import user_key, class_key
 import sqlite3
-from kivy.clock import Clock
 from kivymd.uix.card import MDCard
 from kivymd.uix.dialog import MDDialog
-from kivymd.utils import asynckivy
-from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
-import cv2
-import pyzbar.pyzbar as pyzbar
 from datetime import date
 import winsound
 
@@ -124,54 +119,3 @@ class HomeScreen(Screen):
             self.qr_day = "---"
             self.time_in = "00:00:00"
             self.time_out = "00:00:00"
-
-    # def scan(self):
-
-    #     cap = cv2.VideoCapture(0)
-        
-    #     names=[]
-    
-    #     def checkData(data):
-    #         data=str(data)    
-    #         if data in names:
-    #             pass
-    #             print('Already Scanned')
-    #         else:
-    #             winsound.Beep(frequency, duration)
-    #             print(data)
-    #             names.append(data)
-    #             new_z = data[2:-1]
-    #             split_info = new_z.split(';')
-    #             scanned_student_names.append(split_info)
-      
-    #     while True:
-    #         _, frame = cap.read() 
-    #         decodedObjects = pyzbar.decode(frame)
-    #         for obj in decodedObjects:
-    #             checkData(obj.data)
-    #             time.sleep(1)
-           
-    #         cv2.imshow("Frame", frame)
-
-    #         if cv2.waitKey(1)& 0xFF == ord('s'):
-    #             cv2.destroyAllWindows()
-    #             break
-
-    #     print(scanned_student_names)
-    #     app = MDApp.get_running_app()
-    #     app.show_screen("about_app")
-    #     app.show_screen("home_screen")
-
-        # time.sleep(3)
-        # HomeScreen.refresh(self)
-
-    # def try1(self):
-    #     if len(scanned_student_names) != 0:
-    #         num = str(scanned_student_names[0][0])
-    #         # print(num)
-    #         # HomeScreen.refresh(self)
-    #         self.ids.home_screen.stud_num.clear()
-    #         return num
-    #     else:
-    #         num = '000000000'
-    #         return num
