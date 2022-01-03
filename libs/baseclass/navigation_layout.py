175d26acb2f7d918722227f2098a260370b51daa
_jonsonkurt <jonsonkurt@gmail.com>
2022-01-04 03:10:14 +0800

    Fixed user and class key error

--- libs/baseclass/navigation_layout.py
+++ libs/baseclass/navigation_layout.py
@@ -6,12 +6,7 @@ from libs.baseclass import homescreen, add_student, export
 Builder.load_file('./libs/kv/navigation_layout.kv')
 
 class NavLayoutScreen(Screen):
-    # this function clears the user_key list and class_key list
-    def on_leave(self):
-        
-        user_key.user_key.clear()
-        class_key.class_key.clear()
-
+ 
     def add(self):
         add_student.add_student_dialog(self)
 
